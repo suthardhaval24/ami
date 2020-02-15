6bf93315d5e8471f1d25b5121d90ad768faf3519
suthardhaval24 <suthar.d@husky.neu.edu>
2020-02-15 17:34:08 -0500

    Shell Script updated

--- installGuide.sh
+++ installGuide.sh
@@ -1,4 +1,12 @@
+sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt install openjdk-8-jdk
+echo "Installing Java"
+sudo apt-get install -y openjdk-8-jdk
+echo "Java Version"
+java -version
 echo "Java8 Downloaded"
+echo "Installing Tomcat"
+sudo apt-get install -y tomcat8
+echo "Tomcat Installed"
+echo "------------------------------"
 echo "All the dependencies installed"
