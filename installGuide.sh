b72007be3cad8d4b6c7d2a5b0a409732727e5d76
suthardhaval24 <suthar.d@husky.neu.edu>
2020-02-29 14:56:45 -0500

    aws codeDeloy added

--- installGuide.sh
+++ installGuide.sh
@@ -8,8 +8,15 @@ echo "Java8 Downloaded"
 echo "Installing Tomcat"
 sudo apt-get install -y tomcat8
 echo "Tomcat Installed"
+echo "Installing AWS code Deploy"
+sudo apt-get install -y ruby
 sudo apt-get install -y maven
 sudo apt-get install -y unzip
 sudo apt-get install -y wget
+cd /home/ubuntu
+wget https://codedeploy.dhavalsuthar.me.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
 echo "------------------------------"
 echo "All the dependencies installed"
